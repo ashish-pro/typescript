fc3c4f13f18a8d1b6b32f7c472d6503254b5cdca
ashish-pro <ashishbit017@outlook.com>
2024-05-17 17:15:45 +0530

    boolean type

---
+++ src/types/boolean.type.ts
@@ -0,0 +1,16 @@
+// Boolean values are supported by both JavaScript and TypeScript and stored as true/false values.
+
+let booleanValue: boolean = true;
+
+// Literal values
+booleanValue = false;
+
+// expression of true or false
+booleanValue = typeof "abc" == "string";
+booleanValue = 1 > 0;
+
+booleanValue = {};
+booleanValue = "New York";
+
+booleanValue = null;
+booleanValue = undefined;
