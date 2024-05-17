d1504e8b33e7c225224493a3e03da19664306cf2
ashish-pro <ashishbit017@outlook.com>
2024-05-17 17:32:33 +0530

    string type

---
+++ src/types/string.type.ts
@@ -0,0 +1,11 @@
+// String is another primitive data type that is used to store text data. String values are surrounded by single quotation marks or double quotation marks.
+
+let firstName: string = "Mark";
+
+let fullName: string = firstName + " " + "Doe";
+
+fullName.split(" ");
+
+//fullName = 123456;
+
+Math.log(fullName);
